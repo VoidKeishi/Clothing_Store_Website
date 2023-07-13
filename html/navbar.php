43b350a75a68ec470ca40ea0f7e735cd8eb35bfe
PhucPhuc <daosyphuc288@gmail.com>
2023-07-13 21:30:08 +0700

    Delete home.html and add 3 php files

---
+++ html/navbar.php
@@ -0,0 +1,262 @@
+<div class="contained" id="header-frame">
+    <div id="header-left">
+    <ul>
+        <li>
+            <a href="..//html/index.html" class="name" style="position: relative; margin-left: 25px">
+                <img src="..//img/Logo.svg" alt="Logo" id="logo-img" />
+            </a>
+        </li>
+        <div style="width: 50px; display: inline-block"></div>
+        <div id="header-li">
+            <li>
+                <a href="" class="header-left1 name"><span class="green-underline-text">Nữ</span></a>
+                <!-- Nữ -->
+                <div class="flyout-content">
+                    <div class="flyout-box">
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Áo</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả áo</a></li>
+                                <li><a href="">Áo Thun</a></li>
+                                <li><a href="">Áo sơ mi</a></li>
+                                <li><a href="">Áo nỉ & Hoodie</a></li>
+                                <li><a href="">Áo len</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Quần</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả quần</a></li>
+                                <li><a href="">Quần Jean</a></li>
+                                <li><a href="">Quần dài</a></li>
+                                <li><a href="">Quần tây</a></li>
+                                <li><a href="">Quần short</a></li>
+                                <li><a href="">Quần nỉ</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Đồ mặc ngoài</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả đồ mặc ngoài</a></li>
+                                <li><a href="">Áo khoác & Áo Blazer</a></li>
+                                <li><a href="">Áo khoác siêu nhẹ</a></li>
+                                <li><a href="">Áo Blouson & Áo Parka</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Đồ mặc trong</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả đồ mặc trong</a></li>
+                                <li><a href="">Áo mặc trong</a></li>
+                                <li><a href="">Bra Tops</a></li>
+                                <li><a href="">Đồ lót</a></li>
+                                <li><a href="">Đồ giữ nhiệt</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col" id="fl-right" style="width: 20%">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Nổi Bật</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Hàng mới về</a></li>
+                                <li><a href="">Yêu thích</a></li>
+                                <li><a href="">Sắp ra mắt</a></li>
+                            </ul>
+                            <div class="col-header" style="margin-top: 30%">
+                                <span class="flyout-col-header">Đặc Biệt</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả bộ sưu tập</a></li>
+                                <li><a href="">Bộ sưu tập theo mùa</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="" class="header-left1 name"><span class="green-underline-text">Nam</span></a>
+                <!-- Nam -->
+                <div class="flyout-content">
+                    <div class="flyout-box">
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Áo</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả áo</a></li>
+                                <li><a href="">Áo Thun</a></li>
+                                <li><a href="">Áo sơ mi</a></li>
+                                <li><a href="">Áo nỉ & Hoodie</a></li>
+                                <li><a href="">Áo Polo</a></li>
+                                <li><a href="">Áo len</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Quần</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả quần</a></li>
+                                <li><a href="">Quần Jean</a></li>
+                                <li><a href="">Quần dài</a></li>
+                                <li><a href="">Quần tây</a></li>
+                                <li><a href="">Quần short</a></li>
+                                <li><a href="">Quần nỉ</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Đồ mặc ngoài</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả đồ mặc ngoài</a></li>
+                                <li><a href="">Áo khoác & Áo Blazer</a></li>
+                                <li><a href="">Áo khoác siêu nhẹ</a></li>
+                                <li><a href="">Áo Blouson & Áo Parka</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Đồ mặc trong</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả đồ mặc trong</a></li>
+                                <li><a href="">Áo mặc trong</a></li>
+                                <li><a href="">Đồ giữ nhiệt</a></li>
+                                <li><a href="">Đồ lót</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col" id="fl-right" style="width: 20%">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Nổi Bật</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Hàng mới về</a></li>
+                                <li><a href="">Yêu thích</a></li>
+                                <li><a href="">Sắp ra mắt</a></li>
+                            </ul>
+                            <div class="col-header" style="margin-top: 30%">
+                                <span class="flyout-col-header">Đặc Biệt</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả bộ sưu tập</a></li>
+                                <li><a href="">Bộ sưu tập theo mùa</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="" class="header-left1 name"><span class="green-underline-text">Trẻ Em</span></a>
+                <!-- Trẻ Em -->
+                <div class="flyout-content">
+                    <div class="flyout-box">
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Áo</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả áo</a></li>
+                                <li><a href="">Áo Thun</a></li>
+                                <li><a href="">Áo sơ mi</a></li>
+                                <li><a href="">Áo nỉ & Hoodie</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Quần</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả quần</a></li>
+                                <li><a href="">Quần dài</a></li>
+                                <li><a href="">Quần short</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Đồ mặc ngoài</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả đồ mặc ngoài</a></li>
+                                <li><a href="">Áo khoác</a></li>
+                                <li><a href="">Áo Blouson & Áo Parka</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Đồ mặc trong</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả đồ mặc trong</a></li>
+                                <li><a href="">Áo mặc trong</a></li>
+                                <li><a href="">Đồ giữ nhiệt</a></li>
+                                <li><a href="">Đồ lót</a></li>
+                            </ul>
+                        </div>
+                        <div class="flyout-col" id="fl-right" style="width: 20%">
+                            <div class="col-header">
+                                <span class="flyout-col-header">Nổi Bật</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Hàng mới về</a></li>
+                                <li><a href="">Yêu thích</a></li>
+                                <li><a href="">Sắp ra mắt</a></li>
+                            </ul>
+                            <div class="col-header" style="margin-top: 30%">
+                                <span class="flyout-col-header">Đặc Biệt</span>
+                            </div>
+                            <ul>
+                                <li><a href="">Tất cả bộ sưu tập</a></li>
+                                <li><a href="">Bộ sưu tập theo mùa</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </div>
+    </ul>
+</div>
+<div id="header-right">
+    <ul>
+        <li>
+            <a style="cursor: pointer" id="search-icon"><i class="bi bi-search"></i></a>
+        </li>
+        <li>
+            <a href=""><i class="bi bi-heart"></i></a>
+        </li>
+        <li>
+            <a href=""><i class="bi bi-bag-heart"></i></a>
+        </li>
+        <li>
+            <a href="..//html/login.html"><i class="bi bi-file-person"></i></a>
+        </li>
+    </ul>
+</div>
+</div>
+<div id="search-bar" class="contained">
+    <div id="search-frame">
+        <div id="search-lable-frame" style="margin-left: 50px">
+            <form style="position: relative">
+                <label for="#search-input">
+                    <input type="text" id="search-input" placeholder="Tìm kiếm theo từ khóa" />
+                </label>
+            </form>
+        </div>
+        <a href="..//html/search.html" style="display: inline-block; margin-left: 16px">
+            <button type="submit" id="search-button" title="Tìm kiếm">
+                <i class="bi bi-search" id="search-button-icon"></i>
+            </button>
+        </a>
+        <button type="button" title="close" id="close-button">
+            <i class="bi bi-x-lg" style="color: black; font-size: 30px"></i>
+        </button>
+    </div>
+</div>
